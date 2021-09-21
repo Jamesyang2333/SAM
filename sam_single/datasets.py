fcbf963f4349cdfe942dbfa8bd1745774a4365af
Jamesyang2333 <jamesyang2333@gmail.com>
2021-09-21 13:31:17 +0800

    modified code

--- sam_single/datasets.py
+++ sam_single/datasets.py
@@ -6,7 +6,7 @@ import numpy as np
 import common
 
 def LoadDmv(filename='Vehicle__Snowmobile__and_Boat_Registrations.csv'):
-    csv_file = '../../datasets/{}'.format(filename)
+    csv_file = '../datasets/{}'.format(filename)
     cols = [
         'Record Type','Registration Class', 'State', 'County', 'Body Type',
         'Fuel Type', 'Reg Valid Date', 'Color', 'Scofflaw Indicator',
@@ -19,7 +19,7 @@ def LoadDmv(filename='Vehicle__Snowmobile__and_Boat_Registrations.csv'):
     return common.CsvTable('DMV', csv_file, cols, type_casts)
 
 def LoadCensus(filename='census.csv'):
-    csv_file = '../../datasets/{}'.format(filename)
+    csv_file = '../datasets/{}'.format(filename)
     cols =[0, 1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
     type_casts = {}
     return common.CsvTable('Adult', csv_file, cols, type_casts, header=None)
