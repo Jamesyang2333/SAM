47ba4b25cad1568f291f84754b1827d74e384fd3
jamesyang2333 <jamesyang2333@gmail.com>
2022-07-26 07:57:59 +0000

    upload single relation datasets

--- scripts/download_imdb.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -ex
-
-mkdir -p datasets/job && pushd datasets/job
-wget -c http://homepages.cwi.nl/~boncz/job/imdb.tgz && tar -xvzf imdb.tgz && popd
-
-python3 scripts/prepend_imdb_headers.py
